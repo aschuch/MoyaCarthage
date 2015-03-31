ad43619727dce555073ba50bc9bedbbc12c6938c
Alexander Schuch <alexander@schuch.me>
2015-03-31 10:34:48 +0200

    initial commit

---
+++ Moya/Moya.h
@@ -0,0 +1,19 @@
+//
+//  Moya.h
+//  Moya
+//
+//  Created by Alexander Schuch on 31/03/15.
+//  Copyright (c) 2015 Alexander Schuch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Moya.
+FOUNDATION_EXPORT double MoyaVersionNumber;
+
+//! Project version string for Moya.
+FOUNDATION_EXPORT const unsigned char MoyaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Moya/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# Moya Carthage
+
+The [Moya](https://github.com/ashfurrow/Moya) network abstraction layer library optimized for Carthage.
+
+```
+github "aschuch/MoyaCarthage" "0.1"
+```
+
+This version currently only supports swift 1.2.
+
+# License
+
+Moya was created by [Ash Furrow](https://github.com/ashfurrow) and is released under the [MIT license](https://github.com/ashfurrow/Moya/blob/master/LICENSE).
